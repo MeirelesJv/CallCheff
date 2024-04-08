8d3fee68383025b3e30f9944d7c428117d1a59ef
MATHEUS-HX-ALVES <matheus.xavier.alves@gmail.com>
2024-04-08 18:08:14 -0300

    Commit - Add Carroussel

--- Call Cheff - UI/src/components/carousel/teste.tsx
+++ Call Cheff - UI/src/components/carousel/teste.tsx
@@ -9,12 +9,12 @@ import {
 import useEmblaCarousel from 'embla-carousel-react'
 import './carousel.css'
 
-type PropType = {
+
+type EmblaCarouselProps = {
   slides: string[]
   options?: EmblaOptionsType
 }
-
-const EmblaCarousel: React.FC<PropType> = (props) => {
+const EmblaCarousel: React.FC<EmblaCarouselProps> = (props) => {
   const { slides, options } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options)
 
@@ -33,11 +33,11 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
         {slides.map((src,index) => (
-            <div className="embla__slide bg-white" key={index}>
-              <img className='embla__item' key={index} src={src} alt="" />
-              <div className='embla__item text-dark font-semibold'>Japonedse</div>
+            <div className="embla__slide" key={index}>
+              <img className='' key={index} src={src} alt="" />
             </div>
           ))}
+          
         </div>
       </div>
 
--- Call Cheff - UI/src/main.tsx
+++ Call Cheff - UI/src/main.tsx
@@ -1,19 +1,41 @@
 
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//Styles
 import './styles/global.css'
+
+//Components
+import  EmblaCarousel from './components/carousel'
+import { EmblaOptionsType } from 'embla-carousel'
+
+// Fotos
 import Logo from './assets/svg/Logo.svg'
 import BgFoto from './assets/Bg-Foto.png'
-import fotoArabe from './assets/food/arabe.jpg'
+
+import FTarabe from './assets/food/arabe.jpg'
+import FTchinese from './assets/food/chinese.jpg'
+import FTfeijoada from './assets/food/feijoada.png'
+import FTitaliana from './assets/food/italiana.jpg'
+import FTjaponese from './assets/food/japonese.jpg'
+import FTmexicana from './assets/food/mexicana.jpg'
 
 
+ const images = [
+  FTarabe,
+  FTchinese,
+  FTfeijoada,
+  FTitaliana,
+  FTjaponese,
+  FTmexicana
+]
+const OPTIONS: EmblaOptionsType = { loop: true }
+const SLIDE_COUNT = images.length
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className='flex flex-col'  >
-      <nav className='flex items-center justify-between w-10/12 mx-auto py-4 '>
+      {/* <nav className='flex items-center justify-between w-10/12 mx-auto py-4 '>
         <div className='flex items-center'>
           <img className='mr-4' src={Logo} alt="" />
           <h1 className=' sm:visible invisible '> <a className='font-semibold text-dark' href="#">Call Cheff</a>  </h1>
@@ -22,41 +44,23 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <a className='font-semibold text-dark-orange' href="">Criar Conta</a>
           <button className='font-semibold text-light-grey bg-dark-orange py-2 px-4 rounded-full'>Entrar</button>
         </div>
-      </nav>
+      </nav> */}
 
-      <div className='flex bg-cover bg-no-repeat min-h-96 w-100' style={{ backgroundImage: `url(${BgFoto})` }}>
+      {/* <div className='flex bg-cover bg-no-repeat min-h-96 w-100' style={{ backgroundImage: `url(${BgFoto})` }}>
 
         <div className='flex items-center m-auto h-full w-10/12' >
           <input className='w-80 py-2 px-6 mr-4 rounded-full bg-light-grey text-medium-grey' type="text" placeholder='Insira seu endereço' name="pesquisaEndereço" id="pesquisaEndereço" />
           <button className=' py-2 px-6  rounded-full bg-dark-orange text-light-grey font-semibold'>Pesquisar</button>
         </div>
-      </div >
+      </div > */}
       <div className='flex items-center mx-auto flex-col w-10/12 '>
         <div className='border-b-2 border-solid border-dark my-10'>
           <h3 className='text-dark font-semibold'>Experimente novas culinárias no conforto da sua casa</h3>
         </div>
-        <div>
-          <div className='w-50 bg'>
-            <img className='' src='./assets/food/feijoada.png' />
-            <p>
-              Comida CHinesaadasda
-            </p>
-          </div>
-          <div>
-
-            <p>
-              Comida
-            </p>
-          </div>
-          <div>
-
-            <p>
-              Comida
-            </p>
-          </div>
-        </div>
+    <EmblaCarousel slides={images}  options={OPTIONS}/>
+        
+          
       </div>
-
       {/*
       <div>
         <h3>Como funciona a Call Cheff?</h3>
